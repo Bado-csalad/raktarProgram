d51ebe5d88bad81f0ffd4da27630d49fea3307cf
Badó Máté <bado.mate@outlook.com>
2020-11-03 21:54:45 +0100

    elozo

---
+++ raktarProgram/Data/Filters/UserRoleAdministrationFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace raktarProgram.Data.Filters
+{
+    public class UserRoleAdministrationFilter
+    {
+        public string Kereses { get; set; }
+
+        public List<Tuple<string, string>> Sorrend { get; set; }
+
+        public bool Admin { get; set; }
+        public bool Leader { get; set; }
+        public bool Visitor { get; set; }
+
+    }
+}
